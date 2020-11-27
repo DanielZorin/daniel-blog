7f0bbfb003d05b64e8a3bae98482c9e8de3fb760
Daniel A. Zorin <juan@lvk.cs.msu.su>
2020-11-28 00:15:02 +0300

    Signed-off-by: Daniel A. Zorin <juan@lvk.cs.msu.su>

--- src/components/Header.component.js
+++ src/components/Header.component.js
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom'
 
 function Header() {
     return (
-        <nav>
-            <ul class="menuItems">
+        <nav className="menu">
+            <ul className="menuItems">
                 <li><Link to="/">Путешествия</Link></li>
                 <li><Link to="/map">Карта</Link></li>
                 <li><Link to="/list">Содержание</Link></li>
                 <li><Link to="/plans">Планы</Link></li>
+                <li><Link to="/contacts">Контакты</Link></li>
             </ul>
         </nav>
     )
