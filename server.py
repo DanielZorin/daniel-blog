0e67cebb6ecc9fe689ac70e0822be6a13285dae2
Daniel A. Zorin <juan@lvk.cs.msu.su>
2020-11-28 01:11:00 +0300

    Backend
    Signed-off-by: Daniel A. Zorin <juan@lvk.cs.msu.su>

---
+++ server.py
@@ -0,0 +1,43 @@
+import os
+from threading import Thread
+
+from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
+from flask.json import JSONEncoder
+import time
+
+
+
+app = Flask(__name__, static_folder='build', static_url_path='')
+CORS(app)
+
+@app.route('/', methods=["GET"])
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/favicon.ico', methods=["GET"])
+def favicon():
+    return app.send_static_file('favicon.ico')
+
+# ====================================== GET METHODS ==================================================
+@app.route('/get_trip/<trip>', methods=["GET"])
+def get_trip(trip):
+    try:
+        f = open("backend-data/trip/" + trip + ".html", "r")
+        data = f.read()
+        f.close()
+    except:
+        data = ""
+    return jsonify({'trip': data})
+
+
+@app.route('/get_index', methods=["GET"])
+def get_index():
+    f = open("backend-data/index.html", "r", encoding='utf-8')
+    data = f.read()
+    f.close()
+    return jsonify({'trip': data})
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
