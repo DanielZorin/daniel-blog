26da11fea564d2bc53bffa78a71f1bed2725a994
Daniel A. Zorin <juan@lvk.cs.msu.su>
2020-11-29 19:30:04 +0300

    Display posts
    Signed-off-by: Daniel A. Zorin <juan@lvk.cs.msu.su>

--- src/pages/trip.component.js
+++ src/pages/trip.component.js
@@ -1,7 +1,43 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
+import axios from 'axios'
+
+/* For local debugging set to 1 */
+const DEBUG = 1;
+
+/* Debug variables.*/
+const PREFIX = DEBUG ? "http://localhost:80" : "";
 
 const TripPage = () => {
-    return <div></div>
-} 
+    let { tripId } = useParams();
+    console.log(tripId)
+    const [data, setData] = React.useState(null)
+    let URL = PREFIX + "/get_trip/" + tripId;
+    React.useEffect(() => axios.get(URL).then(res => { setData(res.data.trip) }), [URL]);
+    if (data)
+        console.log(data.content)
+    if (!data)
+        return <div></div>
+    return (
+        <div>
+            <h1>{data.title}</h1>
+            <i>{data.dates}</i>
+            {
+
+                data.content.map((e) => {
+                    switch (e.type) {
+                        case "text":
+                            return <p dangerouslySetInnerHTML={{ __html: e.src }}></p>
+                        case "image":
+                            return <img src={e.src} />
+                        case "section":
+                            return <h2>{e.src}</h2>
+                    }
+                }
+                )
+            }
+        </div>
+    );
+}
 
 export default TripPage;
